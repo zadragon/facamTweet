8435e1703eb03aafccba0aede995eaf34a759e36
zadragon <mase0804@naver.com>
2024-02-05 23:56:19 +0900

    update

--- src/components/posts/PostForm.tsx
+++ src/components/posts/PostForm.tsx
@@ -1,30 +1,65 @@
+import AuthContext from "context/AuthContext";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { useContext, useState } from "react";
 import { FiImage } from "react-icons/fi";
+import { toast } from "react-toastify";
 
 export default function PostForm() {
-  const handleFileUpload = () => {};
+	const [content, setContent] = useState<string>("");
+	const { user } = useContext(AuthContext);
+	const handleFileUpload = () => {};
 
-  return (
-    <form className="post-form">
-      <textarea
-        className="post-form__textarea"
-        required
-        name="content"
-        id="content"
-        placeholder="What is happening?"
-      />
-      <div className="post-form__submit-area">
-        <label htmlFor="file-input" className="post-form__file">
-          <FiImage className="post-form__file-icon" />
-        </label>
-        <input
-          type="file"
-          name="file-input"
-          accept="image/*"
-          onChange={handleFileUpload}
-          className="hidden"
-        />
-        <input type="submit" value="Tweet" className="post-form__submit-btn" />
-      </div>
-    </form>
-  );
+	const onSubmit = async (e: any) => {
+		e.preventDefault();
+		try {
+			await addDoc(collection(db, "posts"), {
+				content: content,
+				createdAt: new Date()?.toLocaleDateString("ko", {
+					hour: "2-digit",
+					minute: "2-digit",
+					second: "2-digit",
+				}),
+				uid: user?.uid,
+				email: user?.email,
+			});
+			setContent("");
+			toast.success("게시글을 생성했습니다");
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		const {
+			target: { name, value },
+		} = e;
+		console.log(name);
+		console.log(value);
+
+		if (name === "content") {
+			setContent(value);
+		}
+	};
+
+	return (
+		<form className="post-form" onSubmit={onSubmit}>
+			<textarea
+				className="post-form__textarea"
+				required
+				name="content"
+				id="content"
+				value={content}
+				placeholder="What is happening?"
+				onChange={onChange}
+			/>
+			<div className="post-form__submit-area">
+				<label htmlFor="file-input" className="post-form__file">
+					<FiImage className="post-form__file-icon" />
+				</label>
+				<input type="file" name="file-input" accept="image/*" onChange={handleFileUpload} className="hidden" />
+				<input type="submit" value="Tweet" className="post-form__submit-btn" />
+			</div>
+		</form>
+	);
 }
--- src/components/users/SignupForm.tsx
+++ src/components/users/SignupForm.tsx
@@ -1,126 +1,146 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { app } from "firebaseApp";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const SignupForm = () => {
-  const [error, setError] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
-  const navigate = useNavigate();
+	const [error, setError] = useState<string>("");
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
+	const navigate = useNavigate();
 
-  const onSubmit = async (e: any) => {
-    e.preventDefault();
-    try {
-      const auth = getAuth(app);
-      await createUserWithEmailAndPassword(auth, email, password);
-      navigate("/");
-      toast.success("성공적인 회원가입 완료");
-    } catch (e: any) {
-      toast.error("회원가입 실패");
-    }
-  };
+	const onSubmit = async (e: any) => {
+		e.preventDefault();
+		try {
+			const auth = getAuth(app);
+			await createUserWithEmailAndPassword(auth, email, password);
+			navigate("/");
+			toast.success("성공적인 회원가입 완료");
+		} catch (e: any) {
+			toast.error("회원가입 실패");
+		}
+	};
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { name, value },
-    } = e;
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const {
+			target: { name, value },
+		} = e;
 
-    if (name === "email") {
-      setEmail(value);
-      const validRegex =
-        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+		if (name === "email") {
+			setEmail(value);
+			const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-      if (!value?.match(validRegex)) {
-        setError("유효한 이메일을 넣어라");
-      } else {
-        setError("");
-      }
-    }
-    if (name === "password") {
-      setPassword(value);
+			if (!value?.match(validRegex)) {
+				setError("유효한 이메일을 넣어라");
+			} else {
+				setError("");
+			}
+		}
+		if (name === "password") {
+			setPassword(value);
 
-      if (value?.length < 8) {
-        setError("비밀번호는 8자 이상 써주세요");
-      } else if (value !== passwordConfirmation) {
-        setError("비밀번호와 비밀번호 확인값이 다릅니다.");
-      } else {
-        setError("");
-      }
-    }
-    if (name === "password__confirmation") {
-      setPasswordConfirmation(value);
+			if (value?.length < 8) {
+				setError("비밀번호는 8자 이상 써주세요");
+			} else if (value !== passwordConfirmation) {
+				setError("비밀번호와 비밀번호 확인값이 다릅니다.");
+			} else {
+				setError("");
+			}
+		}
+		if (name === "password__confirmation") {
+			setPasswordConfirmation(value);
 
-      if (value?.length < 8) {
-        setError("비밀번호는 8자 이상 써주세요");
-      } else if (password !== value) {
-        setError("비밀번호와 비밀번호 확인값이 다릅니다.");
-      } else {
-        setError("");
-      }
-    }
-  };
-  return (
-    <form className="form form--lg" onSubmit={onSubmit}>
-      <div className="form__title">회원가입</div>
-      <div className="form__block">
-        <label htmlFor="email">이메일</label>
-        <input
-          type="text"
-          name="email"
-          id="email"
-          value={email}
-          onChange={onChange}
-          required
-        />
-      </div>
-      <div className="form__block">
-        <label htmlFor="password">비밀번호</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={onChange}
-          id="password"
-          required
-        />
-      </div>
-      <div className="form__block">
-        <label htmlFor="password__confirmation">비밀번호 확인</label>
-        <input
-          type="password"
-          name="password__confirmation"
-          id="password__confirmation"
-          value={passwordConfirmation}
-          onChange={onChange}
-          required
-        />
-      </div>
-      {error && error?.length > 0 && (
-        <div className="form__block">
-          <div className="form__error">{error}</div>
-        </div>
-      )}
+			if (value?.length < 8) {
+				setError("비밀번호는 8자 이상 써주세요");
+			} else if (password !== value) {
+				setError("비밀번호와 비밀번호 확인값이 다릅니다.");
+			} else {
+				setError("");
+			}
+		}
+	};
 
-      <div className="form__block">
-        계정이 있으신가요?
-        <Link to="/users/login" className="form__link">
-          로그인하기
-        </Link>
-      </div>
-      <div className="form__block--lg">
-        <button
-          type="submit"
-          className="form__btn--submit"
-          disabled={error.length > 0}
-        >
-          회원가입
-        </button>
-      </div>
-    </form>
-  );
+	const onClickSocialLogin = async (e: any) => {
+		const {
+			target: { name },
+		} = e;
+
+		let provider;
+		const auth = getAuth(app);
+
+		if (name === "google") {
+			provider = new GoogleAuthProvider();
+		}
+
+		if (name === "github") {
+			provider = new GithubAuthProvider();
+		}
+
+		await signInWithPopup(auth, provider as GithubAuthProvider | GoogleAuthProvider)
+			.then((result) => {
+				console.log(result);
+				toast.success("로그인 되었습니다.");
+			})
+			.catch((error: any) => {
+				console.log(error);
+				const errorMessage = error?.message;
+				toast.error(errorMessage);
+			});
+		console.log(name);
+	};
+	return (
+		<form className="form form--lg" onSubmit={onSubmit}>
+			<div className="form__title">회원가입</div>
+			<div className="form__block">
+				<label htmlFor="email">이메일</label>
+				<input type="text" name="email" id="email" value={email} onChange={onChange} required />
+			</div>
+			<div className="form__block">
+				<label htmlFor="password">비밀번호</label>
+				<input type="password" name="password" value={password} onChange={onChange} id="password" required />
+			</div>
+			<div className="form__block">
+				<label htmlFor="password__confirmation">비밀번호 확인</label>
+				<input
+					type="password"
+					name="password__confirmation"
+					id="password__confirmation"
+					value={passwordConfirmation}
+					onChange={onChange}
+					required
+				/>
+			</div>
+			{error && error?.length > 0 && (
+				<div className="form__block">
+					<div className="form__error">{error}</div>
+				</div>
+			)}
+
+			<div className="form__block">
+				계정이 있으신가요?
+				<Link to="/users/login" className="form__link">
+					로그인하기
+				</Link>
+			</div>
+			<div className="form__block--lg">
+				<button type="submit" className="form__btn--submit" disabled={error.length > 0}>
+					회원가입
+				</button>
+			</div>
+			<div className="form__block">
+				<button type="button" name="google" className="form__btn--google" onClick={onClickSocialLogin}>
+					구글로 회원가입
+				</button>
+			</div>
+			<div className="form__block">
+				<button type="button" name="github" className="form__btn--github" onClick={onClickSocialLogin}>
+					깃헙으로 회원가입
+				</button>
+			</div>
+		</form>
+	);
 };
 
 export default SignupForm;
